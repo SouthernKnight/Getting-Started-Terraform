cffa6eea6a85ad1701c3625a5ada900c4306edcc
Robert Bland <rob.bland@vumc.org>
2022-01-13 16:26:20 -0600

    Removed AWS keys

--- globo_web_app/main.tf
+++ globo_web_app/main.tf
@@ -3,9 +3,7 @@
 ##################################################################################
 
 provider "aws" {
-  #access_key = "AKIASDTAPIRKIQISGDU5"
   access_key = var.aws_access_key
-  #secret_key = "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"
   secret_key = var.aws_secret_key
   region     = var.aws_region
 }
